bd4ca74d5f6cdd9bb8c3019433d014c72493cf9f
Neal Bakanas <nealbakanas@gmail.com>
2019-03-10 19:54:45 -0400

    Update stack.py

--- stack.py
+++ stack.py
@@ -16,33 +16,3 @@ class Stack:
         return len(self.items)
     def __str__(self):
         return str(self.items)
-
-
-
-
-class Queue:
-    def __init__(self):
-        self.items = []
-
-    def is_empty(self):
-        return self.items == []
-
-    def push(self,item):
-        self.items.append(item)
-
-    def pop(self):
-        remove = self.items[0]
-        self.items.remove(remove)
-        return remove
-
-    def __str__(self):
-        return str(self.items)
-
-queue1 = Queue()
-
-queue1.push(1)
-queue1.push(3)
-queue1.push(5)
-print (queue1)
-queue1.pop()
-print (queue1)
